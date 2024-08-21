2776ea02bf79595b06c8cb8f04c0324fb23f40d8
Arif-Rpl <mishbahulmaarif972@gmail.com>
2024-08-21 19:38:16 +0700

    Add files via upload
    masih jelek Pakk

---
+++ WebSMP/Index.php
@@ -0,0 +1,51 @@
+
+<?php
+    require_once "style.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website SMPN 2 BUDURAN</title>
+    <link rel="stylesheet" href="desain.css">
+    <link rel="shrotcut icon" style="x-icon" href="img/logo.png">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <img src="img/logo.png" height="100" width="100">
+            <h1>SMPN 2 BUDURAN</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Beranda</a></li>
+                    <li><a href="#">Tentang Kami</a></li>
+                    <li><a href="#">Program Studi</a></li>
+                    <li><a href="#">Kontak</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main class="container">
+        <section>
+            <h2 style="text-align: center;">Selamat Datang di Sekolah Kami!</h2>
+            <p style="text-align: center;">Ini adalah halaman utama dari website sekolah kami. Kami menyediakan informasi lengkap mengenai sekolah, program studi, dan banyak lagi.</p>
+        </section>
+
+        <section>
+            <h2 style="padding-top: 20px;">Berita Terbaru</h2>
+             <a href="#"><img src="img/bali.jpg" alt="" height="400" width="600"></a>
+             <p>Liburan ke Bali</p>
+        </section>
+    </main>
+    
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2024 Website Sekolah. Semua Hak Dilindungi.</p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ WebSMP/style.php
@@ -0,0 +1,3 @@
+<?php
+    $logo="logo.png";
+?>
