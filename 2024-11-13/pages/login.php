43801219d15b9c0f19d9ca7897564741b18aab45
Arif-Rpl <mishbahulmaarif972@gmail.com>
2024-11-13 14:02:12 +0700

    Add files via upload

---
+++ 2024-11-13/pages/login.php
@@ -0,0 +1,30 @@
+<div class="login">
+    <h1>login</h1>
+    <form action="" method="post">
+        <input type="email" name="email" require placeholder="Masukkan email">
+        <input type="password" name="password" require placeholder="Masukkan password">
+        <input type="submit" name="login" value="login">
+    </form>
+</div>
+
+<?php
+    if (isset($_POST['login'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $sql = "SELECT * FROM customer WHERE email = '$email' AND PASSWORD = '$password'";
+        // echo $sql;
+        $hasil = mysqli_query($koneksi, $sql);
+        $baris = mysqli_num_rows($hasil);
+        // echo $baris;
+        if ($baris > 0) {
+            $_SESSION['email'] = $email;
+            header("location:index.php");
+        }
+        else {
+            echo "<h1>Email atau password salah</h1>";
+
+        }
+    
+    }
+   
+?>
